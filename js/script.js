61ca93d8d498c0fa2525adff7c55ac87fcb5bf5b
Nycollas <sobolevski.academy@gmail.com>
2023-01-10 13:32:54 -0300

    sla

---
+++ js/script.js
@@ -0,0 +1,43 @@
+function AbrirCadastro(id) {
+    CleanScreen()
+    document.getElementById(id).style.display = 'flex'
+    var elemento = document.getElementById(id)
+    if(elemento.style.display == 'none') {
+        document.getElementById('Sala').classList.remove('Blur')
+    }
+    if(elemento.style.display == 'flex') {
+        document.getElementById('Sala').classList.add('Blur')
+    }
+}
+function FecharCadastro(id) {
+    document.getElementById(id).style.display = 'none'
+    CleanScreen()
+}
+function CleanScreen() {
+    document.getElementById('CadastroAluno').style.display = 'none'
+    document.getElementById('CadastroSala').style.display = 'none'
+    document.getElementById('Sala').classList.remove('Blur');
+}
+
+
+let photo = document.getElementById('ProfileAvatar');
+let file  = document.getElementById('flImage');
+
+photo.addEventListener('click', () =>{
+    file.click();
+});
+
+    file.addEventListener('change',() => {
+        if(file.files.lenght == 0)
+        {
+            return;
+        }
+
+        let reader = new FileReader();
+
+        reader.readAsDataURL(file.files[0]);
+
+        reader.onload = () => {
+            photo.src = reader.result
+        }
+    })
