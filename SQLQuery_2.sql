771e76cfd5549e389f391f84f0005699d6923e0f
Nycollas <sobolevski.academy@gmail.com>
2023-01-13 14:33:36 -0300

    UPDATE: atualizado para sala de alunos dinamico (faltando apenas optar por curso)

--- SQLQuery_2.sql
+++ SQLQuery_2.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE Controle_de_Estudantes
+
 -- ------------------ tcp/ip porta -----------------
 SELECT value_data
 FROM sys.dm_server_registry
@@ -8,3 +10,12 @@ AND NULLIF(value_data, '') IS NOT NULL
 
 select * from Salas
 insert  INTO Salas (nome, capacidade, updateAt) VALUES ('TI', 50, 1)
+
+select * from Alunos
+insert into Alunos values('Nycollas', 18, 'Masculino', 'profile_avatar.jpg', GetDate(),GetDate(), 5)
+insert into Alunos values('Petter', 20, 'Masculino', 'gato-e-carnivoro2.jpg', GetDate(),GetDate(), 4)
+insert into Alunos values('Lucas', 18, 'Masculino', 'Bosch-Logo2.png', GetDate(),GetDate(), 6)
+
+
+update Alunos 
+set Foto = 'profile_avatar.jpg' where IDAluno = 1
--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,14 +1,40 @@
 const sala = require('../model/sala');
+const aluno = require('../model/aluno');
+const { sala } = require('./cadastro');
 
 module.exports = {
     async pagInicialGet(req, res) {
+        const id = req.body.nome;
 
         const salas = await sala.findAll({
             raw: true,  // Retorna somente os valores de uma tabela, sem metadados
             atributes: ['IDSala', 'Nome']
         });
 
+        const alunos = await aluno.findAll({
+            raw: true,
+            atributes: ['Nome', 'Idade', 'Foto', 'IDSala']
+        });
         // renderizando as salas para o front
-        res.render('../views/index.ejs', { salas });
+        res.render('../views/index.ejs', {salas, alunos:'', id:''})
+    },
+
+    async pagInicialPost(req, res){
+        const id = req.body.nome;
+
+        const alunos = await aluno.findAll({
+            raw: true,
+            atributes: ['IDAluno', 'Nome','Idade','Foto'],
+            where: {IDSala: id}
+        });''
+
+        const sala = await sala.findAll({
+            raw:true,
+            atributes: ['IDSala','Nome']
+        });
+
+        res.render('../views/index.ejs', {salas, alunos, id})
     }
+
+    
 }
